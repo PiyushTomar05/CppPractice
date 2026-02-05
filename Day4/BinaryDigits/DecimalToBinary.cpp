a56afbb0432db9e0576b60114115bc7bd929aef6
Piyush_Tomar <piyushtomarp@gmail.com>
2026-02-05 21:36:02 +0530

    Daily C++ practice - 2026-02-05

--- Day4/BinaryDigits/DecimalToBinary.cpp
+++ Day4/BinaryDigits/DecimalToBinary.cpp
@@ -1,11 +1,22 @@
 #include <iostream>
 using namespace std;
 int dectoBinary(int n){
-    int pow=1,
+    int pow=1,ans=0;
+    while(n>0){
+        int rem=n%2;
+        n/=2;
+
+        ans+=(rem*pow);
+        pow*=10;
+    }
 
     return ans;
 }
 int main() {
+    int n=50;
+    
+
+    cout<<dectoBinary(n);
     
     return 0;
 }
