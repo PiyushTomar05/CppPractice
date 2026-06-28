8693a12e2996a98db65595aa55762b9ec21fc526
Piyush_Tomar <piyushtomarp@gmail.com>
2026-06-28 23:42:59 +0530

    Update GetIthBit implementation

--- Day9/GetIthBit.cpp
+++ Day9/GetIthBit.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 using namespace std;
-
+int GetIthBit(int n,int i){
+    int bitmask=1<<i;
+    if(n&bitmask ==0){
+        return 0;
+    }else{
+        return 1;
+    }
+}
 int main() {
+    cout<<GetIthBit(6,2)<<endl;
+    cout<<GetIthBit(7,1)<<endl;
     
     return 0;
 }
